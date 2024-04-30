9158e5386bcccf48ab2407c0f617644158e06820
Anjasfedo <fedoaf98@gmail.com>
2024-04-30 19:14:04 +0700

    Create APIController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
 end
+
+class APIController < ActionController::API
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < APIController
   before_action :set_user, only: %i[ show update destroy ]
 
   # GET /users
